660f30b382fa71fa1bc3eb464a64f0358b841df3
Walter Nkosinathi Magagula <nkosinathiwalter001@gmail.com>
2024-12-30 18:54:46 +0200

    add s3 bucket construct

--- Cloud/IaC/aws-cdk/my-cdk-app/lib/my-cdk-app-stack.ts
+++ Cloud/IaC/aws-cdk/my-cdk-app/lib/my-cdk-app-stack.ts
@@ -1,16 +1,40 @@
-import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { 
+  Stack, 
+  StackProps,
+  RemovalPolicy, 
+  aws_s3 as s3,
+  aws_iam as iam
+} from 'aws-cdk-lib';
 
-export class MyCdkAppStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class MyCdkAppStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+    
+    const bucket = new s3.Bucket(this, 'new-s3-bucket-00', {
+      enforceSSL: true,
+      minimumTLSVersion: 1.2,
+      removalPolicy: RemovalPolicy.DESTROY
+    });
 
-    // The code that defines your stack goes here
+    const bucketPolicy = new iam.PolicyStatement({
+      actions: ['s3:*'],
+      resources: ['*'],
+      principals: [new iam.AccountRootPrincipal()]
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'MyCdkAppQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const addPolicyResults = bucket.addToResourcePolicy(bucketPolicy);
+
+    if (addPolicyResults.statementAdded) {
+      console.log("===============================================================================");
+      console.log("policy statement added!");
+    } else {
+      console.log("===============================================================================");
+      console.log("policy statement not added!");
+    }
+
+    console.log("bucketName: ", bucket.bucketName);
+    console.log("website url: ", bucket.bucketWebsiteUrl); 
+    console.log("===============================================================================");
   }
 }
