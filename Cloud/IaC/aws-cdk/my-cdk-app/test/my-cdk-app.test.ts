d26332785e4cba75477ef73c911a7587ad74c7f8
Walter Nkosinathi Magagula <nkosinathiwalter001@gmail.com>
2024-12-30 19:01:36 +0200

    add tests for s3 bucket construct

--- Cloud/IaC/aws-cdk/my-cdk-app/test/my-cdk-app.test.ts
+++ Cloud/IaC/aws-cdk/my-cdk-app/test/my-cdk-app.test.ts
@@ -1,17 +1,13 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as MyCdkApp from '../lib/my-cdk-app-stack';
+import { App } from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import { MyCdkAppStack } from '../lib/my-cdk-app-stack';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/my-cdk-app-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new MyCdkApp.MyCdkAppStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+test('S3 Bucket Created', () => {
+    const app = new App();
+    // WHEN
+    const stack = new MyCdkAppStack(app, 'MyTestStack');
+    // THEN
+    const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+    template.hasResourceProperties('AWS::S3::Bucket', {});
 });
