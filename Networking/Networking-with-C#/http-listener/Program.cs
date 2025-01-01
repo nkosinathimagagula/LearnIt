455ccde2204518e5cad226c4cb5c79370167faac
Walter Nkosinathi Magagula <nkosinathiwalter001@gmail.com>
2025-01-01 22:45:46 +0200

    add http listener

---
+++ Networking/Networking-with-C#/http-listener/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace HttpListenerExample
+{
+    class HttpServer
+    {
+        public static string url = "http://localhost:8000/";
+        public static int pageViews = 0;
+        public static int requestCount = 0;
+        public static string pageData =
+            "<!DOCTYPE>" +
+            "<html>" +
+            "  <head>" +
+            "    <title>HttpListener Example</title>" +
+            "  </head>" +
+            "  <body>" +
+            "    <p>Page Views: {0}</p>" +
+            "    <form method=\"post\" action=\"shutdown\">" +
+            "      <input type=\"submit\" value=\"Shutdown\" {1}>" +
+            "    </form>" +
+            "  </body>" +
+            "</html>";
+
+        public static async Task HandleIncomingConnections(HttpListener listener)
+        {
+            bool runServer = true;
+
+            // While a user hasn't visited the `shutdown` url, keep on handling requests
+            while (runServer)
+            {
+                // Will wait here until we hear from a connection
+                HttpListenerContext ctx = await listener.GetContextAsync();
+
+                // Peel out the requests and response objects
+                HttpListenerRequest request = ctx.Request;
+                HttpListenerResponse response = ctx.Response;
+
+                // Print out some info about the request
+                Console.WriteLine("Request #: {0}", ++requestCount);
+                Console.WriteLine(request.Url.ToString());
+                Console.WriteLine(request.HttpMethod);
+                Console.WriteLine(request.UserHostName);
+                Console.WriteLine(request.UserAgent);
+                Console.WriteLine();
+
+                // If `shutdown` url requested w/ POST, then shutdown the server after serving the page
+                if ((request.HttpMethod == "POST") && (request.Url.AbsolutePath == "/shutdown"))
+                {
+                    Console.WriteLine("Shutdown requested");
+                    runServer = false;
+                }
+
+                // Make sure we don't increment the page views counter if `favicon.ico` is requested
+                if (request.Url.AbsolutePath != "/favicon.ico")
+                {
+                    pageViews++;
+                }
+
+                // Write the response info
+                string disableSubmit = !runServer ? "disabled" : "";
+                byte[] data = Encoding.UTF8.GetBytes(String.Format(pageData, pageViews, disableSubmit));
+                response.ContentType = "text/html";
+                response.ContentEncoding = Encoding.UTF8;
+                response.ContentLength64 = data.LongLength;
+
+                // Write out to the response stream (asynchronously), then close it
+                await response.OutputStream.WriteAsync(data, 0, data.Length);
+                response.Close();
+            }
+        }
+
+        public static void Main(String[] args)
+        {
+            // Create a Http server and start listening for incoming connections
+            HttpListener listener = new HttpListener();
+            listener.Prefixes.Add(url);
+            listener.Start();
+            Console.WriteLine("Listening for connections on {0}: ", url);
+
+            // Handle requests
+            Task listenTask = HandleIncomingConnections(listener);
+            listenTask.GetAwaiter().GetResult();
+
+            // Close the listener
+            listener.Close();
+        }
+    }
+}
