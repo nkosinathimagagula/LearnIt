def17aaba81186cde60fad779f776199bc632dad
Nkosinathi Magagula <nkosinathiwalter001@gmail.com>
2026-01-01 22:16:35 +0200

    add database migration

---
+++ Mobile/ReactNative/RecipeApp/backend/src/database/migrations/0000_windy_the_captain.sql
@@ -0,0 +1,10 @@
+CREATE TABLE "favourites" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"user_id" integer NOT NULL,
+	"recipe_id" integer NOT NULL,
+	"title" text NOT NULL,
+	"image" text,
+	"cook_time" integer,
+	"servings" integer,
+	"created_at" timestamp DEFAULT now()
+);
