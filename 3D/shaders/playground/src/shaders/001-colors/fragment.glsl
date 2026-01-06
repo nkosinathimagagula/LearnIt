49089be09c3bcbde4819b19ba385587ba7bb1b76
Nkosinathi Magagula <nkosinathiwalter001@gmail.com>
2026-01-06 21:28:41 +0200

    update colors fragment shader

--- 3D/shaders/playground/src/shaders/001-colors/fragment.glsl
+++ 3D/shaders/playground/src/shaders/001-colors/fragment.glsl
@@ -4,9 +4,10 @@ uniform vec2 uMouse;
 
 void main()
 {
-    // vec2 st = gl_FragCoord.xy/uResolution.xy;
-    vec2 normalizedMousePosition = uMouse.xy / uResolution.xy;
+    vec2 st = gl_FragCoord.xy/uResolution.xy;
+    // vec2 normalizedMousePosition = uMouse.xy / uResolution.xy;
     float bTime = sin(uTime * 10.0);
     
-    gl_FragColor = vec4(normalizedMousePosition.x, normalizedMousePosition.y, bTime, 1.0);
+    gl_FragColor = vec4(st.x, st.y, 1.0, 1.0);
+    // gl_FragColor = vec4(normalizedMousePosition.x, normalizedMousePosition.y, bTime, 1.0);
 }
