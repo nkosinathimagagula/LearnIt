8b37fb8b7a97632f5cb72379a718bf6e605a8fdb
Nkosinathi Magagula <nkosinathiwalter001@gmail.com>
2026-01-14 22:47:10 +0200

    add custom safe area component

---
+++ Mobile/ReactNative/RecipeApp/frontend/app/components/SafeScreen.tsx
@@ -0,0 +1,19 @@
+import { COLORS } from "@/constants/colors";
+import { View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+export default function SafeScreen({ children }: { children: React.ReactNode }) {
+    const insets = useSafeAreaInsets()
+
+    return (
+        <View
+            style={{
+                paddingTop: insets.top,
+                flex: 1,
+                backgroundColor: COLORS.background,
+            }}
+        >
+            {children}
+        </View>
+    );
+}
