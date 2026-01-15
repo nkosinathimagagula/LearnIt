c3f0e52eb6b1af95b3a730aabad70a092090c561
Nkosinathi Magagula <nkosinathiwalter001@gmail.com>
2026-01-15 21:19:52 +0200

    update app layout

--- Mobile/ReactNative/RecipeApp/frontend/app/_layout.tsx
+++ Mobile/ReactNative/RecipeApp/frontend/app/_layout.tsx
@@ -2,20 +2,14 @@ import { Slot, Stack } from "expo-router";
 import { StatusBar } from "react-native";
 import { ClerkProvider } from "@clerk/clerk-expo";
 import { tokenCache } from "@clerk/clerk-expo/token-cache";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { COLORS } from "@/constants/colors";
+import SafeScreen from "./components/SafeScreen";
 
 export default function RootLayout() {
   return (
     <ClerkProvider tokenCache={tokenCache}>
-      <SafeAreaView
-        style={{
-          flex: 1,
-          backgroundColor: COLORS.background
-        }}
-      >
+      <SafeScreen>
         <Slot />
-      </SafeAreaView>
+      </SafeScreen>
     </ClerkProvider>
   );
 }
