8b47478b1d4029f7875f1a684b6889597a4da5f8
Nkosinathi Magagula <nkosinathiwalter001@gmail.com>
2026-01-16 23:26:09 +0200

    add tabs

--- Mobile/ReactNative/RecipeApp/frontend/app/(tabs)/_layout.tsx
+++ Mobile/ReactNative/RecipeApp/frontend/app/(tabs)/_layout.tsx
@@ -1,5 +1,7 @@
+import { COLORS } from "@/constants/colors"
 import { useAuth } from "@clerk/clerk-expo"
-import { Redirect, Stack } from "expo-router"
+import { Ionicons } from "@expo/vector-icons"
+import { Redirect, Tabs } from "expo-router"
 
 const TabsLayout = () => {
     const { isSignedIn } = useAuth()
@@ -7,7 +9,49 @@ const TabsLayout = () => {
     if (!isSignedIn) return <Redirect href={"/(auth)/sign-in"} />
 
     return (
-        <Stack />
+        <Tabs
+            screenOptions={{
+                headerShown: false,
+                tabBarActiveTintColor: COLORS.primary,
+                tabBarInactiveTintColor: COLORS.textLight,
+                tabBarStyle: {
+                    backgroundColor: COLORS.white,
+                    borderTopColor: COLORS.textLight,
+                    borderTopWidth: 1,
+                    paddingBottom: 8,
+                    paddingTop: 8,
+                    height: 80,
+                },
+                tabBarLabelStyle: {
+                    fontSize: 12,
+                    fontWeight: "600",
+                },
+            }}
+        >
+            <Tabs.Screen
+                name="index"
+                options={{
+                    title: "Recipes",
+                    tabBarIcon: ({ color, size }: { color: string, size: number }) => <Ionicons name="restaurant" size={size} color={color} />
+                }}
+            />
+
+            <Tabs.Screen
+                name="search"
+                options={{
+                    title: "Search",
+                    tabBarIcon: ({ color, size }: { color: string, size: number }) => <Ionicons name="search" size={size} color={color} />
+                }}
+            />
+
+            <Tabs.Screen
+                name="favourites"
+                options={{
+                    title: "Favourites",
+                    tabBarIcon: ({ color, size }: { color: string, size: number }) => <Ionicons name="heart" size={size} color={color} />
+                }}
+            />
+        </Tabs>
     )
 }
 
