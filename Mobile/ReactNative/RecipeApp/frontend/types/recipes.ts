65f5f32a70057a40304af6e5e454a929ba1115d9
Nkosinathi Magagula <nkosinathiwalter001@gmail.com>
2026-01-19 21:45:23 +0200

    add useDebounce hook

---
+++ Mobile/ReactNative/RecipeApp/frontend/types/recipes.ts
@@ -0,0 +1,14 @@
+export type TransformedMealType = {
+  id: number;
+  title: string;
+  description: string;
+  image: string;
+  cookTime: string;
+  servings: number;
+  category: string;
+  area: string;
+  ingredients: string[];
+  instructions: string;
+  video: string;
+  originalData: any;
+};
