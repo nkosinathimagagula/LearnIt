32d1ee4c4768cd9c45a0e896eedc1bbdb7c2186f
Nkosinathi Magagula <nkosinathiwalter001@gmail.com>
2026-02-08 10:03:52 +0200

    rename uniforms to match typescript code

--- 3D/shaders/playground/src/shaders/001-colors/fragment.glsl
+++ 3D/shaders/playground/src/shaders/001-colors/fragment.glsl
@@ -1,12 +1,12 @@
-uniform vec2 uResolution;
-uniform float uTime;
-uniform vec2 uMouse;
+uniform vec2 u_resolution;
+uniform float u_time;
+uniform vec2 u_mouse;
 
 void main()
 {
-    vec2 st = gl_FragCoord.xy/uResolution.xy;
-    // vec2 normalizedMousePosition = uMouse.xy / uResolution.xy;
-    float bTime = sin(uTime * 10.0);
+    vec2 st = gl_FragCoord.xy/u_resolution.xy;
+    // vec2 normalizedMousePosition = u_mouse.xy / u_resolution.xy;
+    float bTime = sin(u_time * 10.0);
     
     gl_FragColor = vec4(st.x, st.y, 1.0, 1.0);
     // gl_FragColor = vec4(normalizedMousePosition.x, normalizedMousePosition.y, bTime, 1.0);
