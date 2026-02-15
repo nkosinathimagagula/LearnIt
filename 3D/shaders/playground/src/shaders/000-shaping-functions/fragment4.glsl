0476e81f153fcfb50e863b7dc1a427457ffeecef
Nkosinathi Magagula <nkosinathiwalter001@gmail.com>
2026-02-15 09:21:30 +0200

    add fragment shader 4 in shaping functions

---
+++ 3D/shaders/playground/src/shaders/000-shaping-functions/fragment4.glsl
@@ -0,0 +1,26 @@
+precision mediump float;
+
+uniform vec2 u_resolution;
+uniform vec2 u_mouse;
+uniform float u_time;
+
+// Plot a line on Y using a value between 0.0 - 1.0
+float plot(vec2 st, float pct) {
+    return smoothstep(pct - 0.02, pct, st.y) - smoothstep(pct, pct + 0.02, st.y );
+}
+
+void main() {
+    vec2 st = gl_FragCoord.xy / u_resolution;
+
+    // Smooth interpolation between 0.1 and 0.9
+    float y = smoothstep(0.1,0.9,st.x);
+    // float y = smoothstep(0.2,0.5,st.x) - smoothstep(0.5,0.8,st.x);
+
+    vec3 color = vec3(y);
+
+    // Plot the line
+    float pct = plot(st, y);
+    color = (1.0 - pct) * color + pct * vec3(0.0, 1.0, 0.0);
+
+    gl_FragColor = vec4(color, 1.0);
+}
