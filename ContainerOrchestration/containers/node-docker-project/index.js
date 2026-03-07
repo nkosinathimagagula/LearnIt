a8e0a19139ab72ca50726b4c4475f835742b81ad
Nkosinathi Magagula <nkosinathiwalter001@gmail.com>
2026-03-07 18:33:38 +0200

    update endpoints and dependencies

--- ContainerOrchestration/containers/node-docker-project/index.js
+++ ContainerOrchestration/containers/node-docker-project/index.js
@@ -2,9 +2,13 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello from inside the container!");
+    res.status(200).json({ message: "Hello from inside the container!" });
+});
+
+app.get("/health", (req, res) => {
+    res.status(200).json({ status: "Healthy" });
 });
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log("Server is running on port 3000");
+    console.log("Server is running on port: 3000");
 });
