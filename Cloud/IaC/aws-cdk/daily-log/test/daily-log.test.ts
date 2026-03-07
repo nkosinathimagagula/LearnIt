45d83148b14de27b7e449e1950340676de5d31b9
Nkosinathi Magagula <nkosinathiwalter001@gmail.com>
2026-03-07 18:37:48 +0200

    update daily-log lambda

---
+++ Cloud/IaC/aws-cdk/daily-log/test/daily-log.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DailyLog from '../lib/daily-log-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/daily-log-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DailyLog.DailyLogStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
