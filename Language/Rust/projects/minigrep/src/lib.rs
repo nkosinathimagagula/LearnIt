f4d4d68265b60ee1ff9a02fcd0ebc4e06c5e3529
Nkosinathi Magagula <nkosinathiwalter001@gmail.com>
2026-08-25 19:37:42 +0200

    update minigrep with iterators ®️

--- Language/Rust/projects/minigrep/src/lib.rs
+++ Language/Rust/projects/minigrep/src/lib.rs
@@ -18,6 +18,7 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     Ok(())
 } 
 
+#[derive(Debug)]
 pub struct Config {
     pub query: String,
     pub filename: String,
@@ -25,13 +26,17 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
+    pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
+        args.next();
 
-        let query = args[1].clone();
-        let filename = args[2].clone();
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a filename"),
+        };
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
 
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
@@ -40,15 +45,10 @@ impl Config {
 }
 
 pub fn search<'a>(query: &'a str, contents: &'a str) -> Vec<&'a str> {
-    let mut results: Vec<&str> = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-    
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &'a str, contents: &'a str) -> Vec<&'a str> {
